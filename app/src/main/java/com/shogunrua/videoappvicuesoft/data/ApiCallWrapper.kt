a064fff34f8837aad948af409903459d26a1c082
Sergei Miroshkin <miroshkinsergei.3@gmail.com>
2022-11-25 14:58:06 +0300

    feat: setup ktor client

---
+++ app/src/main/java/com/shogunrua/videoappvicuesoft/data/ApiCallWrapper.kt
@@ -0,0 +1,28 @@
+package com.shogunrua.videoappvicuesoft.data
+
+import android.util.Log
+import arrow.core.Either
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class ApiCallWrapper @Inject constructor() {
+
+    suspend fun <T> safeApiCall(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        apiCall: suspend () -> T,
+    ): Either<Failure, T> {
+
+        return withContext(dispatcher) {
+            Either.catch {
+                apiCall.invoke()
+            }.mapLeft {
+                Log.e(ApiCallWrapper::class.simpleName, it.stackTraceToString())
+                responseExceptionError()
+            }
+        }
+    }
+
+    private fun responseExceptionError() = Failure
+}
